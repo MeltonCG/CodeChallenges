4fa7abb534d4e9b16baf7e2c89abde7dfc7bcfa8
Matt Felton <28576614+MeltonCG@users.noreply.github.com>
2022-08-12 15:14:20 +0100

    Add files via upload

---
+++ Refactoring Code Challenge - My Attempt/App.Tests/CustomerServiceShould.cs
@@ -0,0 +1,139 @@
+﻿using AutoFixture;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace App.Tests
+{
+    [TestFixture]
+    public sealed class CustomerServiceShould
+    {
+        private static Fixture fixture;
+        private static Mock<ICompanyRepository> companyRepo;
+        private static Mock<ICustomerCreditService> creditService;
+        private static Mock<ICustomerDataAccessWrapper> customerDataAccessWrapper;
+
+        public CustomerServiceShould()
+        {
+            fixture = new Fixture();
+            companyRepo = new Mock<ICompanyRepository>();
+            creditService = new Mock<ICustomerCreditService>();
+            customerDataAccessWrapper = new Mock<ICustomerDataAccessWrapper>();
+        }
+
+        [Test]
+        [TestCase(null, false)]
+        [TestCase("", false)]
+        [TestCase("Joe", true)]
+        public void AddCustomer_FirstName(string firstName, bool expectedResult)
+        {
+            // Arrange
+            string surname = "Bloggs";
+            string email = "Joe.Bloggs@adomain.com";
+            DateTime dateOfBirth = new DateTime(1980, 3, 27);
+            int companyId = 123;
+            var company = fixture.Create<Company>();
+            
+            MockSetupForBasicValidation();
+
+            // Act
+            var instance = new CustomerService(companyRepo.Object, creditService.Object, customerDataAccessWrapper.Object);
+            var result = instance.AddCustomer(firstName, surname, email, dateOfBirth, companyId);
+
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+              
+
+        [Test]
+        [TestCase(null, false)]
+        [TestCase("", false)]
+        [TestCase("Bloggs", true)]
+        public void AddCustomer_Surname(string surname, bool expectedResult)
+        {
+            // Arrange
+            string firstName = "Joe";
+            string email = "Joe.Bloggs@adomain.com";
+            DateTime dateOfBirth = new DateTime(1980, 3, 27);
+            int companyId = 123;
+
+
+            MockSetupForBasicValidation();
+
+            // Act
+            var instance = new CustomerService(companyRepo.Object, creditService.Object, customerDataAccessWrapper.Object);
+            var result = instance.AddCustomer(firstName, surname, email, dateOfBirth, companyId);
+
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        [TestCase(null, false)]
+        [TestCase("", false)]
+        [TestCase("Joe.Bloggs invalid email", false)]
+        [TestCase("Joe.Bloggs@adomain.com", true)]
+        public void AddCustomer_Email(string email, bool expectedResult)
+        {
+            // Arrange
+            string firstName = "Joe";
+            string surname = "Bloggs";
+            DateTime dateOfBirth = new DateTime(1980, 3, 27);
+            int companyId = 123;
+
+
+            MockSetupForBasicValidation();
+
+            // Act
+            var instance = new CustomerService(companyRepo.Object, creditService.Object, customerDataAccessWrapper.Object);
+            var result = instance.AddCustomer(firstName, surname, email, dateOfBirth, companyId);
+
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        [TestCase(16, false)]
+        [TestCase(18, false)]
+        [TestCase(20, false)]
+        [TestCase(20, false)]
+        [TestCase(21, true)]
+        [TestCase(25, true)]
+        public void AddCustomer_AgeCheck(int customerAge, bool expectedResult)
+        {
+            // Arrange
+            string firstName = "Joe";
+            string surname = "Bloggs";
+            string email = "Joe.Bloggs@adomain.com";
+            DateTime dateOfBirth = DateTime.Now.AddYears(customerAge * -1).Date;
+            int companyId = 123;
+
+
+            MockSetupForBasicValidation();
+
+            // Act
+            var instance = new CustomerService(companyRepo.Object, creditService.Object, customerDataAccessWrapper.Object);
+            var result = instance.AddCustomer(firstName, surname, email, dateOfBirth, companyId);
+
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        // Ran out of time at this point.
+        // Going forward I would have created unit tests for VeryImportantClient & ImportantClient
+        // checking that the combinations of HasCreditLimit & CreditLimit return the right outcome
+        // e.g. VeryImportantClient has credit limit < 500 but still passes as HasCreditLimit = false
+
+        private static void MockSetupForBasicValidation()
+        {
+            companyRepo.Setup(s => s.GetById(It.IsAny<int>()))
+                            .Returns(fixture.Create<Company>());
+            creditService.Setup(s => s.GetCreditLimit(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<DateTime>()).Result)
+                .Returns(1000);
+        }
+    }
+}
---
+++ Refactoring Code Challenge - My Attempt/App/CustomerService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App
+{
+    public class CustomerService
+    {
+        private readonly ICompanyRepository companyRepository;
+        private readonly ICustomerCreditService customerCreditService;
+        private readonly ICustomerDataAccessWrapper customerDataAccess;
+
+        public CustomerService()
+        {
+            this.companyRepository = new CompanyRepository();
+            this.customerCreditService = new CustomerCreditServiceClient();
+            this.customerDataAccess = new CustomerDataAccessWrapper();
+        }
+
+        public CustomerService(ICompanyRepository companyRepository,
+                               ICustomerCreditService customerCreditService, 
+                               ICustomerDataAccessWrapper customerDataAccess)
+        {
+            this.companyRepository = companyRepository;
+            this.customerCreditService = customerCreditService;
+            this.customerDataAccess = customerDataAccess;
+        }
+
+        public bool AddCustomer(string firstName, string surname, string email, DateTime dateOfBirth, int companyId)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(surname))
+            {
+                return false;
+            }
+
+            // Given more time a method to validate an email address rather than just checking it contains @ and . at any point
+            // in the string
+            if (string.IsNullOrEmpty(email) || !email.Contains("@") || !email.Contains("."))
+            {
+                return false;
+            }
+
+            if (dateOfBirth.Date > DateTime.Now.AddYears(-21).Date)
+            {
+                return false;
+            }
+
+            var company = companyRepository.GetById(companyId);
+
+            var customer = new Customer
+                               {
+                                   Company = company,
+                                   DateOfBirth = dateOfBirth,
+                                   EmailAddress = email,
+                                   Firstname = firstName,
+                                   Surname = surname
+                               };
+
+            if (company.Name == "VeryImportantClient")
+            {
+                // Skip credit check
+                customer.HasCreditLimit = false;
+            }
+            else if (company.Name == "ImportantClient")
+            {
+                // Do credit check and double credit limit
+                customer.HasCreditLimit = true;
+                var creditLimit = customerCreditService.GetCreditLimit(customer.Firstname, customer.Surname, customer.DateOfBirth).Result;
+                creditLimit *= 2;
+                customer.CreditLimit = creditLimit;
+            }
+            else
+            {
+                // Do credit check
+                customer.HasCreditLimit = true;
+                var creditLimit = customerCreditService.GetCreditLimit(customer.Firstname, customer.Surname, customer.DateOfBirth).Result;
+                customer.CreditLimit = creditLimit;
+            }
+
+            if (customer.HasCreditLimit && customer.CreditLimit < 500)
+            {
+                return false;
+            }
+
+            customerDataAccess.AddCustomer(customer);
+
+            return true;
+        }
+    }
+}
